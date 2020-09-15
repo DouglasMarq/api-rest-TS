7862210bdd697299fcfe817dd1164532c55727f8
DouglasMarquesAlves <douglas.marques@55pbx.com.br>
2020-09-15 11:29:03 -0300

    Atualizando pacotes e revertendo banco

--- src/bin/container.ts
+++ src/bin/container.ts
@@ -3,14 +3,19 @@ import { Container } from "inversify";
 import Server from "../server";
 import Router from "../api";
 import Mongo from "../database";
+import { Logger } from "winston";
 
 let container = new Container();
 
 export class bindContainers {
   loadContainers() {
+
+    // let logger = newLogger(config.get("logger.file"));
+
     container.bind<Server>(Server).to(Server).inSingletonScope();
     container.bind<Router>(Router).to(Router).inSingletonScope();
     container.bind<Mongo>(Mongo).to(Mongo).inSingletonScope();
+    // container.bind<Logger>("logger").toConstantValue(logger);
   }
 
   getContainer() {
