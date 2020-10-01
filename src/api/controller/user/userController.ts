4717216143e93504e6fd46eed0694fd2624bf2dd
DouglasMarq <douglas.marq.alves@outlook.com>
2020-09-30 21:01:21 -0300

    att server

---
+++ src/api/controller/user/userController.ts
@@ -0,0 +1,55 @@
+import { Request, Response } from "express";
+import UserSchema from "../../model/db/userSchema";
+import BaseController from ".././base";
+import * as _ from 'lodash';
+
+export default class UserController extends BaseController<UserSchema> {
+    constructor() {
+        super(UserSchema);
+    }
+
+    public async findUser(req: Request, res: Response) {
+        try {
+            return res.status(200).json({'message:': await this.getEntity(req)});
+        } catch (e) {
+            return res.status(404).json({'message': e.message, 'stack': e.stack});
+        }
+    }
+
+    public async createUser(req: Request, res: Response) { 
+        try {
+            let result = await this.createEntity(req);
+            return res.status(_.get(result, 'code') || 404).json({'message:': _.get(result, 'message')});
+        } catch (e) {
+            return res.status(404).json({'message': e.message, 'stack': e.stack});
+        }
+        // let result = schemaValidator.validateCreateSchema({'username': req.body.username, 'password': req.body.password});
+        // if(result.error) {
+        //     return res.status(400).json({"Error": result.error.details[0].message});
+        // }
+        // let verify = await User.findOne({'username': req.body.username});
+        // console.log(verify);
+        // if (!verify) {
+        //     User.create({'username': req.body.username, 'password': req.body.password});
+        // } else {
+        //     return res.status(401).json({statusCode: 401, message: 'Usuário já existente.'});
+        // }
+        return res.status(200).json({ statusCode: 200, message: 'Usuário Criado com sucesso!' });
+    }
+
+    public async deleteUser(req: Request, res: Response) {         
+        try {
+            return res.status(200).json({'message:': await this.deleteEntity(req)});
+        } catch (e) {
+            return res.status(404).json({'message': e.message, 'stack': e.stack});
+        }
+    }
+
+    public async updateUser(req: Request, res: Response) {         
+        try {
+            return res.status(200).json({'message:': await this.updateEntity(req)});
+        } catch (e) {
+            return res.status(404).json({'message': e.message, 'stack': e.stack});
+        }
+    }
+}
---
+++ src/api/schemas/index.ts
@@ -0,0 +1,12 @@
+import 'reflect-metadata';
+import baseSchema from './base';
+
+export default class Schemas<T> extends baseSchema<T>{
+  constructor(type: new() => T) {
+    super(type);
+  }
+
+  public async validateSchema(obj: any) {
+      return await this.validateBaseSchema(obj);
+  }
+}
